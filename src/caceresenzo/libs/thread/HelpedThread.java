55ca5eca70f21f249f10ef05ddd860531674bda9
Caceresenzo <caceresenzo1502@gmail.com>
2018-08-11 23:13:07 +0200

    Created HelperThread (extends Thread but with some useful functions)

---
+++ src/caceresenzo/libs/thread/HelpedThread.java
@@ -0,0 +1,46 @@
+package caceresenzo.libs.thread;
+
+public abstract class HelpedThread extends Thread {
+	
+	private boolean running, cancelled;
+	
+	@Override
+	public void run() {
+		running(true);
+		
+		onRun();
+		
+		if (running && !cancelled) {
+			running(false);
+		}
+	}
+	
+	protected abstract void onRun();
+	
+	public boolean isRunning() {
+		return running;
+	}
+	
+	private void running(boolean state) {
+		running = state;
+		
+		if (!state) {
+			onFinished();
+		}
+	}
+	
+	public boolean isCancelled() {
+		return cancelled;
+	}
+	
+	public void cancel() {
+		cancelled = true;
+		running = false;
+		onCancelled();
+	}
+	
+	protected abstract void onFinished();
+	
+	protected abstract void onCancelled();
+	
+}
